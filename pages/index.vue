44b89dde1b5444acf92ea4ac7ac44f2000b0c3f9
Jesús García <ctw@ctwhome.com>
2020-07-30 12:28:54 +0200

    Nuxt app

--- pages/index.vue
+++ pages/index.vue
@@ -122,11 +122,6 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
 li .nuxt-link-active {
   font-weight: bold;
   color: white;
